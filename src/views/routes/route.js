94bc94a68c6f2acaa6d0c4aa4a0d7848c79ff328
mchau98 <nguynminhchau98@gmail.com>
2024-11-24 16:53:32 +0700

    begin1

--- src/views/routes/route.js
+++ src/views/routes/route.js
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import LandingPage from "../pages/landing-page/landingPage";
 import Register from "../pages/register";
+import Begin1 from "../pages/begin/begin1";
 
 const AllRoutes = () => {
   return (
     <Routes>
       <Route path="/landing" element={<LandingPage />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/begin_1" element={<Begin1 />} />
     </Routes>
   );
 };
