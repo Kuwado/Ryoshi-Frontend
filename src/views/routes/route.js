2f7267312687ca695b7dd08047ca1986f50eaf2b
Tran Trung Anh <trantrunganh279@gmail.com>
2024-11-24 17:50:40 +0700

    Login UI

--- src/views/routes/route.js
+++ src/views/routes/route.js
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import LandingPage from "../pages/landing-page/landingPage";
 import Register from "../pages/register";
+import Login from "../pages/login";
 
 const AllRoutes = () => {
   return (
     <Routes>
       <Route path="/landing" element={<LandingPage />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 };
