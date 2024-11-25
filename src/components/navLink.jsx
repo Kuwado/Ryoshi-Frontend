e7b646397d82cf2f34462fd47780740ce7a083ee
Tran Trung Anh <trantrunganh279@gmail.com>
2024-11-25 11:43:27 +0700

    Change the structure of the header and footer

--- src/components/navLink.jsx
+++ src/components/navLink.jsx
@@ -1,11 +1,18 @@
 import {Link, useLocation} from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./header.css";
+import {
+    fa3,
+    faN,
+    faC,
+    faS
+}from "@fortawesome/free-solid-svg-icons";
 
 const links = [
-    {name: "三地域の観光地", href: "/tourist-attractions-in-three-areas"},
-    {name: "北部の観光地", href: "/tourist-attractions-in-northern-area"},
-    {name: "中部の観光地", href: "/tourist-attractions-in-central-area"},
-    {name: "南部の観光地", href: "/tourist-attractions-in-southern-area"},
+    {name: "三地域の観光地", href: "/tourist-attractions-in-three-areas", icon: fa3},
+    {name: "北部の観光地", href: "/tourist-attractions-in-northern-area", icon: faN},
+    {name: "中部の観光地", href: "/tourist-attractions-in-central-area", icon: faC},
+    {name: "南部の観光地", href: "/tourist-attractions-in-southern-area", icon: faS},
 ]
 
 export default function NavLink() {
@@ -15,6 +22,7 @@ export default function NavLink() {
     return(
         <>
             {links.map((link) => {
+                const Icon = link.icon;
                 return (
                     <Link
                         key={link.href}
@@ -22,6 +30,7 @@ export default function NavLink() {
                         className={`nav-link ${pathname === link.href ? "active" : ""} menu d-flex align-items-center`}
                     >
                         <div className="menu-item d-flex align-items-center">
+                            <FontAwesomeIcon icon={Icon} className="menu-icon" />
                             {link.name}
                         </div>
                     </Link>
