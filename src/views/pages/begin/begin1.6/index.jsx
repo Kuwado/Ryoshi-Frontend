a652f9cf6cac512cc0baa3a3fc6fb3d6c98ad99f
anhthunguyenngoc <anhthunguyenngoc80@gmail.com>
2024-11-26 13:26:43 +0700

    update begins

--- src/views/pages/begin/begin1.6/index.jsx
+++ src/views/pages/begin/begin1.6/index.jsx
@@ -8,16 +8,7 @@ function Begin1_6() {
   const [children_ages, setChildrenAges] = useState([]); // Trạng thái chứa giá trị của các chip đã chọn
 
   const handleChipClick = (ageRange) => {
-    console.log(`Clicked on: ${children_ages}`);
-    setChildrenAges((prevSelected) => {
-      if (prevSelected.includes(ageRange)) {
-        // Nếu chip đã được chọn, bỏ chọn
-        return prevSelected.filter((item) => item !== ageRange);
-      } else {
-        // Nếu chip chưa được chọn, thêm vào danh sách
-        return [...prevSelected, ageRange];
-      }
-    });
+    setChildrenAges([ageRange]); // Đặt trạng thái thành mảng chỉ chứa chip được chọn
   };
 
   const ageRanges = [
@@ -44,7 +35,7 @@ function Begin1_6() {
     // Tạo body cho yêu cầu API
     const body = {
       address: address + ", " + town + ", " + ward + ", " + city, 
-      children_ages: children_ages
+      children_ages: children_ages[0]
     }
 
     try {
@@ -66,7 +57,7 @@ function Begin1_6() {
       const data = await response.json();
 
       // Nếu gửi thành công, chuyển đến trang tiếp theo
-      navigate("/next-step");
+      navigate("/user/begin2");
     } catch (error) {
       console.error('Có lỗi xảy ra khi gửi dữ liệu:', error);
       // Xử lý lỗi nếu cần
@@ -74,56 +65,55 @@ function Begin1_6() {
   };
 
   return (
-    <div className="begin1-6-container">
-      <div className="begin1-6-header">
-        <h1 className="left-title">あなたの位置を選択してください</h1>
-        <h1 className="right-title">
-          関心のあるお子様の年齢を選択してください
-        </h1>
-      </div>
-
+    <div className="begin1-container">
       <div className="begin1-6-content">
-        <div className="begin1-6-form-section">
-          <form>
-            <div className="begin1-6-form-item">
-              <input type="text" placeholder="県/市" className="input-field" 
-              value={city}
-                onChange={(e) => setCity(e.target.value)} // Cập nhật giá trị city
-              />
-            </div>
-            <div className="begin1-6-form-item">
-              <input type="text" placeholder="区/郡" className="input-field" 
-              value={ward}
-                onChange={(e) => setWard(e.target.value)} // Cập nhật giá trị ward
-              />
-            </div>
-            <div className="begin1-6-form-item">
-              <input type="text" placeholder="町/村" className="input-field" 
-              value={town}
-                onChange={(e) => setTown(e.target.value)} // Cập nhật giá trị town
-              />
-            </div>
-            <div className="begin1-6-form-item">
-              <input
-                type="text"
-                placeholder="具体的な住所を入力してください"
-                className="input-field"
-                value={address}
-                onChange={(e) => setAddress(e.target.value)} // Cập nhật giá trị address
-              />
-            </div>
-          </form>
-        </div>
-
-        <div className="begin1-6-options-section">
-          <div className="age-options-grid">
-          {ageRanges.map((ageRange) => (
-              <div
-                key={ageRange}
-                className={`chips-wrapper ${children_ages.includes(ageRange) ? 'selected' : ''}`} // Áp dụng class selected nếu chip được chọn
-                onClick={() => handleChipClick(ageRange)} // Gọi hàm khi nhấn
-              >
+        <div className="flex-item-1">
+          <h1 className="begin1-title">あなたの位置を選択してください</h1>
+          <div className="begin1-6-form-section">
+            <form>
+              <div className="begin1-6-form-item">
+                <input type="text" placeholder="県/市" className="input-field" 
+                value={city}
+                  onChange={(e) => setCity(e.target.value)} // Cập nhật giá trị city
+                />
+              </div>
+              <div className="begin1-6-form-item">
+                <input type="text" placeholder="区/郡" className="input-field" 
+                value={ward}
+                  onChange={(e) => setWard(e.target.value)} // Cập nhật giá trị ward
+                />
+              </div>
+              <div className="begin1-6-form-item">
+                <input type="text" placeholder="町/村" className="input-field" 
+                value={town}
+                  onChange={(e) => setTown(e.target.value)} // Cập nhật giá trị town
+                />
+              </div>
+              <div className="begin1-6-form-item">
                 <input
+                  type="text"
+                  placeholder="具体的な住所を入力してください"
+                  className="input-field"
+                  value={address}
+                  onChange={(e) => setAddress(e.target.value)} // Cập nhật giá trị address
+                />
+              </div>
+            </form>
+          </div>
+        </div>
+        <div className="flex-item-1">
+          <h1 className="begin1-title">関心のあるお子様の年齢を選択してください</h1>
+          <div className="begin1-6-options-section">
+            <div className="age-options-grid">
+              {ageRanges.map((ageRange, index) => (
+                <div
+                  key={index}
+                  className={`chip-fixed-width chips-wrapper ${
+                    children_ages.includes(ageRange) ? "selected" : ""
+                  }`}
+                  onClick={() => handleChipClick(ageRange)}
+                >
+                  <input
                   type="checkbox"
                   id={ageRange}
                   className="d-none"
@@ -131,12 +121,13 @@ function Begin1_6() {
                   readOnly
                 />
                 <label htmlFor={ageRange}>{ageRange}</label>
-              </div>
-            ))}
-          </div>
-          <div class="button-wrapper begin1-6-submit-button" onClick={handleNextClick}>
-            <input type="checkbox" id="button" class="d-none"/>
-            <label for="button" class="btn d-flex align-items-center justify-content-center">次のステップ</label>
+                </div>
+              ))}
+            </div>
+            <div class="button-wrapper begin1-6-submit-button" onClick={handleNextClick}>
+              <input type="checkbox" id="button" class="d-none"/>
+              <label for="button" class="btn d-flex align-items-center justify-content-center">次のステップ</label>
+            </div>
           </div>
         </div>
       </div>
--- src/views/pages/begin/begin2/index.jsx
+++ src/views/pages/begin/begin2/index.jsx
@@ -8,36 +8,26 @@ import Begin1_4 from "../../../../assets/images/begin1.4.png";
 
 function Begin2() {
   const navigate = useNavigate();
+
+  const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Miwicm9sZSI6InVzZXIiLCJpYXQiOjE3MzI1NDU3OTQsImV4cCI6MTczNTEzNzc5NH0.OAkbvzKUhceuKw_PbMPhTtDOVqSHJ2_6Y-wksCpydBg'; // Thay thế bằng token thực tế
+  const userId = 1
+
   const [selectedItems, setSelectedItems] = useState([]);
-
-  const handleItemClick = (item) => {
-    setSelectedItems(
-      (prev) =>
-        prev.includes(item)
-          ? prev.filter((i) => i !== item) // Bỏ chọn nếu đã chọn
-          : [...prev, item] // Chọn thêm mục mới
-    );
-  };
-
-  const handleNextClick = () => {
-    if (selectedItems.length < 5) {
-      alert("最低5つの項目を選択してください");
-    } else {
-      navigate("/next-step");
-    }
-  };
 
   const options = [
     "エコツーリズム",
     "文化旅行",
     "リゾート",
+    "\n",
     "レクリエーション",
     "スポーツ",
     "探検",
     "冒険",
+    "\n",
     "コンビネーション",
     "家族旅行",
     "団体旅行",
+    "\n",
     "個人旅行",
     "ビーチ",
     "山",
@@ -45,38 +35,126 @@ function Begin2() {
     "田舎",
   ];
 
+  const rows = [];
+  let currentRow = [];
+
+  options.forEach((option, index) => {
+    if (option === "\n") {
+      // Nếu gặp ký tự xuống dòng, đẩy currentRow vào rows và reset currentRow
+      rows.push(currentRow);
+      currentRow = []; // Reset currentRow
+    } else {
+      // Thêm option vào currentRow
+      currentRow.push(option);
+    }
+  });
+
+  // Đừng quên thêm currentRow còn lại nếu có
+  if (currentRow.length > 0) {
+    rows.push(currentRow);
+  }
+
+  const handleItemClick = (item) => {
+    setSelectedItems((prevSelected) => {
+      // Nếu chip đã được chọn, bỏ chọn
+      if (prevSelected.includes(item)) {
+        return prevSelected.filter((selectedItem) => selectedItem !== item);
+      } else {
+        // Nếu chip chưa được chọn, thêm vào danh sách nếu số lượng chưa vượt quá 5
+        if (prevSelected.length < 5) {
+          return [...prevSelected, item];
+        }
+        return prevSelected; // Không thêm nếu đã đạt tối đa 5 chip
+      }
+    });
+  };
+
+  const handleNextClick = async (e) => {
+    e.preventDefault(); // Ngăn chặn hành vi mặc định của form
+
+    // Tạo body cho yêu cầu API
+    const body = {
+      interest: selectedItems.toString()
+    }
+
+    try {
+      // Gửi yêu cầu POST đến API
+      const response = await fetch('http://localhost:8000/api/v1/users/' + userId, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`, // Thêm Bearer Token vào header
+        },
+        body: JSON.stringify(body),
+      });
+
+      // Kiểm tra phản hồi từ server
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const data = await response.json();
+
+      // Nếu gửi thành công, chuyển đến trang tiếp theo
+      if (selectedItems.length < 5) {
+        alert("最低5つの項目を選択してください");
+      } else {
+        navigate("/next-step");
+      }
+    } catch (error) {
+      console.error('Có lỗi xảy ra khi gửi dữ liệu:', error);
+      // Xử lý lỗi nếu cần
+    }
+  };
+
   return (
     <div className="begin1-container">
-      <h1 className="begin1-title">興味のある旅行のタイプを選択してください</h1>
-      <h2 className="begin1-subtitle">最低5つの項目を選択してください</h2>
+      <h1 className="begin1-title" style={{ margin: 0 }}>興味のある旅行のタイプを選択してください</h1>
+      <div className="flex-2cols">
+        <div className="flex-col">
+          <h2 className="begin1-title" style={{ marginTop: "10px" }}>最低5つの項目を選択してください</h2>
 
-      <div className="begin1-content">
-        {/* Phần form lựa chọn */}
-        <div className="begin1-form-section">
-          <div className="options-grid">
-            {options.map((option, index) => (
-              <div
-                key={index}
-                className={`option-item ${
-                  selectedItems.includes(option) ? "selected" : ""
-                }`}
-                onClick={() => handleItemClick(option)}
-              >
-                {option}
+          <div className="begin1-content">
+            {/* Phần form lựa chọn */}
+            <div className="begin2-form-section">
+              <div className="options-column full-height">
+              {rows.map((row, rowIndex) => (
+                <div className="options-row" key={rowIndex}>
+                  {row.map((item, itemIndex) => (
+                    <div
+                      className={`chips-wrapper ${selectedItems.includes(item) ? 'selected' : ''}`}
+                      onClick={() => handleItemClick(item)}
+                      key={itemIndex}
+                    >
+                      <input
+                        type="checkbox"
+                        id={`itemIndex`} // Đảm bảo id là duy nhất
+                        className="d-none"
+                        checked={selectedItems.includes(item)}
+                        readOnly
+                      />
+                      <label htmlFor={`checkbox-${rowIndex}-${itemIndex}`}>{item}</label>
+                    </div>
+                  ))}
+                </div>
+              ))}
+                  
               </div>
-            ))}
+              <div class="button-wrapper begin2-submit-button" onClick={handleNextClick}>
+                <input type="checkbox" id="button" class="d-none"/>
+                <label for="button" class="btn d-flex align-items-center justify-content-center">探検を始めよう！</label>
+              </div>
+            </div>
           </div>
-          <button className="begin1-submit-button" onClick={handleNextClick}>
-            探検を始めよう！
-          </button>
         </div>
-
         {/* Phần hình ảnh */}
-        <div className="image-section">
-          <img src={Begin1_1} className="begin1-image" />
-          <img src={Begin1_2} className="begin1-image" />
-          <img src={Begin1_3} className="begin1-image" />
-          <img src={Begin1_4} className="begin1-image" />
+        <div className="flex-item2">
+          <div className="image-section">
+              <img src={Begin1_1} className="begin1-image item-1" />
+              <img src={Begin1_2} className="begin1-image item-2" />
+              <img src={Begin1_3} className="begin1-image item-3" />
+              <img src={Begin1_4} className="begin1-image item-4" />
+          </div>
         </div>
       </div>
     </div>
